b353186505c57c8f516fa99050d94eafb1cffaa5
araledezma <aracelibelenledezma@gmail.com>
2025-05-19 00:16:56 -0300

    cambie a listview de nuevo
    cambie a listview de nuevo

--- 5 CasoU Cargar Orden de Entrega/CargarOrdenDeEntregaForm.Designer.cs
+++ 5 CasoU Cargar Orden de Entrega/CargarOrdenDeEntregaForm.Designer.cs
@@ -28,22 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
-            DataGridViewCellStyle dataGridViewCellStyle3 = new DataGridViewCellStyle();
-            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
+            ListViewItem listViewItem3 = new ListViewItem(new ListViewItem.ListViewSubItem[] { new ListViewItem.ListViewSubItem(null, "OS25001", SystemColors.WindowText, SystemColors.ControlLightLight, new Font("Segoe UI", 8F, FontStyle.Regular, GraphicsUnit.Point, 0)), new ListViewItem.ListViewSubItem(null, "18/05/2025", SystemColors.WindowText, SystemColors.ControlLightLight, new Font("Segoe UI", 8F, FontStyle.Bold, GraphicsUnit.Point, 0)), new ListViewItem.ListViewSubItem(null, "20123456780 - Juan Soto"), new ListViewItem.ListViewSubItem(null, "SKU12345"), new ListViewItem.ListViewSubItem(null, "Producto de ejemplo"), new ListViewItem.ListViewSubItem(null, "8"), new ListViewItem.ListViewSubItem(null, "aledezma"), new ListViewItem.ListViewSubItem(null, "11/04/2025") }, -1);
             ordenesPreparacionEntregaLabel = new Label();
-            button1 = new Button();
-            button2 = new Button();
-            dataGridView1 = new DataGridView();
-            idOrdenSeleccion = new DataGridViewTextBoxColumn();
-            fechaDespacho = new DataGridViewTextBoxColumn();
-            idClienteCuit = new DataGridViewTextBoxColumn();
-            skuProducto = new DataGridViewTextBoxColumn();
-            productoDescripcion = new DataGridViewTextBoxColumn();
-            cantidadProducto = new DataGridViewTextBoxColumn();
-            idOperadorPicking = new DataGridViewTextBoxColumn();
-            fechaPicking = new DataGridViewTextBoxColumn();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            agregarOS = new Button();
+            ordenesPickingCumplidas = new ListView();
+            idOrdenPicking = new ColumnHeader();
+            fechaDespachoOE = new ColumnHeader();
+            cuitCliente = new ColumnHeader();
+            skuProductoPicking = new ColumnHeader();
+            nombreProducto = new ColumnHeader();
+            productoCantidad = new ColumnHeader();
+            idUsuarioPicking = new ColumnHeader();
+            fechaPickingCumplida = new ColumnHeader();
+            borrarSelección = new Button();
             SuspendLayout();
             // 
             // ordenesPreparacionEntregaLabel
@@ -52,133 +49,103 @@
             ordenesPreparacionEntregaLabel.Location = new Point(26, 22);
             ordenesPreparacionEntregaLabel.Margin = new Padding(6, 0, 6, 0);
             ordenesPreparacionEntregaLabel.Name = "ordenesPreparacionEntregaLabel";
-            ordenesPreparacionEntregaLabel.Size = new Size(243, 37);
+            ordenesPreparacionEntregaLabel.Size = new Size(387, 37);
             ordenesPreparacionEntregaLabel.TabIndex = 0;
-            ordenesPreparacionEntregaLabel.Text = "Órden de selección";
+            ordenesPreparacionEntregaLabel.Text = "Órdenes de selección validadas";
             // 
-            // button1
+            // agregarOS
             // 
-            button1.Enabled = false;
-            button1.Location = new Point(1367, 513);
-            button1.Margin = new Padding(6, 7, 6, 7);
-            button1.Name = "button1";
-            button1.Size = new Size(321, 57);
-            button1.TabIndex = 3;
-            button1.Text = "Cargar orden de entrega";
-            button1.UseVisualStyleBackColor = true;
+            agregarOS.Enabled = false;
+            agregarOS.Location = new Point(1270, 286);
+            agregarOS.Margin = new Padding(6, 7, 6, 7);
+            agregarOS.Name = "agregarOS";
+            agregarOS.Size = new Size(230, 67);
+            agregarOS.TabIndex = 3;
+            agregarOS.Text = "Agregar";
+            agregarOS.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // ordenesPickingCumplidas
             // 
-            button2.Location = new Point(1123, 563);
-            button2.Name = "button2";
-            button2.Size = new Size(348, 158);
-            button2.TabIndex = 4;
-            button2.Text = "ESTE ES UN BOTON DE PRUEBA";
-            button2.UseVisualStyleBackColor = true;
-            button2.Click += button2_Click;
+            ordenesPickingCumplidas.BackColor = SystemColors.ControlLightLight;
+            ordenesPickingCumplidas.CheckBoxes = true;
+            ordenesPickingCumplidas.Columns.AddRange(new ColumnHeader[] { idOrdenPicking, fechaDespachoOE, cuitCliente, skuProductoPicking, nombreProducto, productoCantidad, idUsuarioPicking, fechaPickingCumplida });
+            ordenesPickingCumplidas.Font = new Font("Segoe UI Semibold", 8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            ordenesPickingCumplidas.FullRowSelect = true;
+            ordenesPickingCumplidas.GridLines = true;
+            listViewItem3.StateImageIndex = 0;
+            ordenesPickingCumplidas.Items.AddRange(new ListViewItem[] { listViewItem3 });
+            ordenesPickingCumplidas.LabelEdit = true;
+            ordenesPickingCumplidas.Location = new Point(26, 75);
+            ordenesPickingCumplidas.Name = "ordenesPickingCumplidas";
+            ordenesPickingCumplidas.OwnerDraw = true;
+            ordenesPickingCumplidas.Size = new Size(1474, 201);
+            ordenesPickingCumplidas.TabIndex = 6;
+            ordenesPickingCumplidas.UseCompatibleStateImageBehavior = false;
+            ordenesPickingCumplidas.View = View.Details;
             // 
-            // dataGridView1
+            // idOrdenPicking
             // 
-            dataGridView1.BackgroundColor = SystemColors.AppWorkspace;
-            dataGridView1.BorderStyle = BorderStyle.Fixed3D;
-            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle1.BackColor = SystemColors.ControlLight;
-            dataGridViewCellStyle1.Font = new Font("Segoe UI Semibold", 8F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            dataGridViewCellStyle1.ForeColor = SystemColors.ControlText;
-            dataGridViewCellStyle1.NullValue = null;
-            dataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight;
-            dataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText;
-            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
-            dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { idOrdenSeleccion, fechaDespacho, idClienteCuit, skuProducto, productoDescripcion, cantidadProducto, idOperadorPicking, fechaPicking });
-            dataGridView1.Location = new Point(26, 71);
-            dataGridView1.Name = "dataGridView1";
-            dataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle3.BackColor = SystemColors.Control;
-            dataGridViewCellStyle3.Font = new Font("Segoe UI", 8F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            dataGridViewCellStyle3.ForeColor = SystemColors.WindowText;
-            dataGridViewCellStyle3.SelectionBackColor = SystemColors.Highlight;
-            dataGridViewCellStyle3.SelectionForeColor = SystemColors.HighlightText;
-            dataGridViewCellStyle3.WrapMode = DataGridViewTriState.True;
-            dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
-            dataGridView1.RowHeadersWidth = 92;
-            dataGridView1.Size = new Size(1511, 171);
-            dataGridView1.TabIndex = 5;
-            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            idOrdenPicking.Text = "Número";
+            idOrdenPicking.Width = 150;
             // 
-            // idOrdenSeleccion
+            // fechaDespachoOE
             // 
-            dataGridViewCellStyle2.BackColor = Color.White;
-            idOrdenSeleccion.DefaultCellStyle = dataGridViewCellStyle2;
-            idOrdenSeleccion.HeaderText = "Número";
-            idOrdenSeleccion.MinimumWidth = 11;
-            idOrdenSeleccion.Name = "idOrdenSeleccion";
-            idOrdenSeleccion.Resizable = DataGridViewTriState.False;
-            idOrdenSeleccion.Width = 125;
+            fechaDespachoOE.Text = "Fecha de despacho";
+            fechaDespachoOE.Width = 160;
             // 
-            // fechaDespacho
+            // cuitCliente
             // 
-            fechaDespacho.HeaderText = "Fecha de despacho";
-            fechaDespacho.MinimumWidth = 11;
-            fechaDespacho.Name = "fechaDespacho";
-            fechaDespacho.Width = 160;
+            cuitCliente.Text = "CUIT - Cliente";
+            cuitCliente.Width = 300;
             // 
-            // idClienteCuit
+            // skuProductoPicking
             // 
-            idClienteCuit.HeaderText = "CUIT - Cliente";
-            idClienteCuit.MinimumWidth = 11;
-            idClienteCuit.Name = "idClienteCuit";
-            idClienteCuit.Width = 300;
+            skuProductoPicking.Text = "SKU";
+            skuProductoPicking.Width = 160;
             // 
-            // skuProducto
+            // nombreProducto
             // 
-            skuProducto.HeaderText = "SKU";
-            skuProducto.MinimumWidth = 11;
-            skuProducto.Name = "skuProducto";
-            skuProducto.Width = 160;
+            nombreProducto.Text = "Producto";
+            nombreProducto.Width = 225;
             // 
-            // productoDescripcion
+            // productoCantidad
             // 
-            productoDescripcion.HeaderText = "Descripción del producto";
-            productoDescripcion.MinimumWidth = 11;
-            productoDescripcion.Name = "productoDescripcion";
-            productoDescripcion.Width = 225;
+            productoCantidad.Text = "Cantidad";
+            productoCantidad.Width = 125;
             // 
-            // cantidadProducto
+            // idUsuarioPicking
             // 
-            cantidadProducto.HeaderText = "Cantidad";
-            cantidadProducto.MinimumWidth = 11;
-            cantidadProducto.Name = "cantidadProducto";
-            cantidadProducto.Width = 125;
+            idUsuarioPicking.Text = "Preparada por";
+            idUsuarioPicking.Width = 180;
             // 
-            // idOperadorPicking
+            // fechaPickingCumplida
             // 
-            idOperadorPicking.HeaderText = "Preparada por";
-            idOperadorPicking.MinimumWidth = 11;
-            idOperadorPicking.Name = "idOperadorPicking";
-            idOperadorPicking.Width = 160;
+            fechaPickingCumplida.Text = "Fecha de picking";
+            fechaPickingCumplida.Width = 160;
             // 
-            // fechaPicking
+            // borrarSelección
             // 
-            fechaPicking.HeaderText = "Fecha";
-            fechaPicking.MinimumWidth = 11;
-            fechaPicking.Name = "fechaPicking";
-            fechaPicking.Width = 160;
+            borrarSelección.Enabled = false;
+            borrarSelección.Location = new Point(1028, 286);
+            borrarSelección.Margin = new Padding(6, 7, 6, 7);
+            borrarSelección.Name = "borrarSelección";
+            borrarSelección.Size = new Size(230, 67);
+            borrarSelección.TabIndex = 7;
+            borrarSelección.Text = "Borrar selección";
+            borrarSelección.UseVisualStyleBackColor = true;
             // 
             // CargarOrdenDeEntregaForm
             // 
             AutoScaleDimensions = new SizeF(15F, 37F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1542, 755);
-            Controls.Add(dataGridView1);
-            Controls.Add(button2);
-            Controls.Add(button1);
+            ClientSize = new Size(1526, 755);
+            Controls.Add(borrarSelección);
+            Controls.Add(ordenesPickingCumplidas);
+            Controls.Add(agregarOS);
             Controls.Add(ordenesPreparacionEntregaLabel);
             Margin = new Padding(6, 7, 6, 7);
             Name = "CargarOrdenDeEntregaForm";
             Text = "Generar - Órden de Entrega";
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -186,16 +153,16 @@
         #endregion
 
         private Label ordenesPreparacionEntregaLabel;
-        private Button button1;
-        private Button button2;
-        private DataGridView dataGridView1;
-        private DataGridViewTextBoxColumn idOrdenSeleccion;
-        private DataGridViewTextBoxColumn fechaDespacho;
-        private DataGridViewTextBoxColumn idClienteCuit;
-        private DataGridViewTextBoxColumn skuProducto;
-        private DataGridViewTextBoxColumn productoDescripcion;
-        private DataGridViewTextBoxColumn cantidadProducto;
-        private DataGridViewTextBoxColumn idOperadorPicking;
-        private DataGridViewTextBoxColumn fechaPicking;
+        private Button agregarOS;
+        private ListView ordenesPickingCumplidas;
+        private ColumnHeader idOrdenPicking;
+        private ColumnHeader fechaDespachoOE;
+        private ColumnHeader cuitCliente;
+        private ColumnHeader skuProductoPicking;
+        private ColumnHeader nombreProducto;
+        private ColumnHeader productoCantidad;
+        private ColumnHeader idUsuarioPicking;
+        private ColumnHeader fechaPickingCumplida;
+        private Button borrarSelección;
     }
 }
