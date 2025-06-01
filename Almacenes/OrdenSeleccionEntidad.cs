78fc43504d1ab6c1a9b742b37c657bcbfe5f48c0
sebassmartinez <143354364+sebassmartinez@users.noreply.github.com>
2025-06-01 15:11:01 -0300

    almacenes

---
+++ Almacenes/OrdenSeleccionEntidad.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPGrupoE.Almacenes
+{
+    internal class OrdenSeleccionEntidad
+    {
+        public int IdOrdenSeleccion { get; set; }
+       
+        public string Estado { get; set; } = string.Empty;
+        public List<OrdenPreparacionEntidad> ordenesPreparacion { get; set; }
+       
+    }
+}
