a610b6daff5f534c017bcbfc1729ef21c02567b4
brisatames <brisatamess@gmail.com>
2025-06-02 00:16:02 -0300

    emitir remito

--- CasosDeUso/CU8EmitirRemito/Forms/EmitirRemitoForm.cs
+++ CasosDeUso/CU8EmitirRemito/Forms/EmitirRemitoForm.cs
@@ -7,20 +7,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TPGrupoE.CasosDeUso.CU6EmitirRemito.Model;
 
 namespace TPGrupoE.CasoU_Generar_Orden_de_Entrega
 {
     public partial class EmitirRemitoForm : Form
     {
+        private EmitirRemitoModel _model;
         public EmitirRemitoForm()
         {
             InitializeComponent();
+            _model = new EmitirRemitoModel();
+
         }
 
         private void EmitirRemitoForm_Load(object sender, EventArgs e)
         {
-
+            comboBoxTransportistas.Items.Clear();
+            foreach (var t in _model.Transportistas)
+            {
+                comboBoxTransportistas.Items.Add(new ComboBoxItem { Text = t.Nombre, Value = t.DNI.ToString() });
+            }
+            comboBoxClientes.Items.Clear();
+            dataGridViewOPs.DataSource = null;
         }
+
 
         private void label3_Click(object sender, EventArgs e)
         {
---
+++ CasosDeUso/CU8EmitirRemito/Model/EmitirRemitoModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TPGrupoE.Almacenes;
+
+namespace TPGrupoE.CasosDeUso.CU6EmitirRemito.Model
+{
+    internal class EmitirRemitoModel
+    {
+        public List<OrdenPreparacionEntidad> OrdenesDePreparacion { get; private set; } = new();
+        public List<TransportistaEntidad> Transportistas { get; private set; } = new();
+
+        public EmitirRemitoModel()
+        {
+            CargarTransportistas();
+        }
+
+        public void CargarTransportistas()
+        {
+            Transportistas = OrdenPreparacionAlmacen.ObtenerTransportistasConOPPreparadasHoy();
+        }
+
+        public List<ClienteEntidad> ObtenerClientesParaTransportistaHoy(int dniTransportista)
+        {
+            return OrdenPreparacionAlmacen.ObtenerClientesConOPPreparadasHoyPorTransportista(dniTransportista);
+        }
+
+        public void CargarOrdenesPorTransportistaYCliente(int dniTransportista, string cuitCliente)
+        {
+            OrdenesDePreparacion = OrdenPreparacionAlmacen.ObtenerOPPreparadasHoyPorTransportistaYCliente(dniTransportista, cuitCliente);
+        }
+
+        public string DespacharYEmitirRemito(int dniTransportista, string cuitCliente, List<int> idsOPSeleccionadas)
+        {
+            if (idsOPSeleccionadas.Count == 0)
+                return "Debe seleccionar al menos una orden para despachar.";
+
+            foreach (var idOp in idsOPSeleccionadas)
+            {
+                var op = OrdenPreparacionAlmacen.ObtenerPorId(idOp);
+                if (op != null)
+                    op.Estado = "Despachada";
+            }
+
+            var remito = new RemitoEntidad
+            {
+                DNITransportista = dniTransportista,
+                IDCliente = cuitCliente,
+                IDOrdenPreparacion = idsOPSeleccionadas,
+                Fecha = DateTime.Now
+            };
+            RemitoAlmacen.Agregar(remito);
+            RemitoAlmacen.Grabar();
+            OrdenPreparacionAlmacen.Grabar();
+
+            return $"Remito generado con éxito. N°{remito.IDRemito}";
+        }
+    }
+}
+
