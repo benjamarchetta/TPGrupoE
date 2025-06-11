2fdf582d82643b6eee6185b05717fbd3d08d32f6
AgustinDojas <agustindojas@gmail.com>
2025-06-11 15:31:47 -0300

    cambios palletcerrado

--- CasosDeUso/CU5GestionarOrdenDeSeleccion/Model/ProductoDetalleDTO.cs
+++ CasosDeUso/CU5GestionarOrdenDeSeleccion/Model/ProductoDetalleDTO.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TPGrupoE.Almacenes;
 
 namespace TPGrupoE.CasosDeUso.CU5GestionarOrdenDeSeleccion.Model
 {
